e42f4d9435965b329f1f22b957172822aabd1b07
Ahmed El Ghattas <ahmed.ghattas@outlook.com>
2021-01-09 14:04:04 +0200

    Gamma OOP

--- Gam.py
+++ Gam.py
@@ -6,7 +6,7 @@ from scipy.optimize import curve_fit
 plt.style.use('tableau-colorblind10')
 
 class Gamma:
-    def __init__(self):
+    def __init__(self, filename, sheet, scn_cell, mw_cell, mole_fraction_cell, h_parameter=-4):
         self.h = 0
         self.scn_start = 0
         self.scn_plus_start = 0
@@ -22,6 +22,13 @@ class Gamma:
         self.maximum_split = 500
         self.split_fit_mi_full = 0
         self.split_fit_zni_full = 0
+        self.data = pd.read_excel(filename, sheet_name=sheet)
+        self.scn = np.array(self.data[scn_cell])
+        self.mi_lab = np.array(self.data[mw_cell])
+        self.mf_lab = np.array(self.data[mole_fraction_cell])
+        self.h = h_parameter
+        self.characterized(self.scn, self.mi_lab, self.mf_lab, self.h)
+
 
     def gamma_parameters(self, x, alpha_, beta_):
         return gamma.cdf(x / beta_, alpha_)
@@ -39,15 +46,15 @@ class Gamma:
         return _zn_plus_split, _mw_plus_split
 
     def split_error(self, split_limit):
-        zn_plus_cal, b = split(scn_start, scn_plus_start, split_limit, h, alpha, beta, lower_bound)
-        return (zn_plus_cal - zn_plus_lab)**2
+        zn_plus_cal, b = self.split(self.scn_start, self.scn_plus_start, split_limit, self.h, self.alpha, self.beta, self.lower_bound)
+        return (zn_plus_cal - self.zn_plus_lab)**2
 
 
     def optimize_split(self, maximum_split_):
         optimum_ = 0
-        max_split_range = np.arange(scn_plus_start + 3, maximum_split_)
+        max_split_range = np.arange(self.scn_plus_start + 3, maximum_split_)
         for i, j in enumerate(max_split_range):
-            if i > 0 and split_error(j) - split_error(j - 1) > 0:
+            if i > 0 and self.split_error(j) - self.split_error(j - 1) > 0:
                 optimum_ = (j - 1)
                 break
             else:
@@ -73,3 +80,33 @@ class Gamma:
         fit_parameters, fit_convergence = curve_fit(self.gamma_parameters, x_cdf_char, zni_cum_lab)
         self.alpha = fit_parameters[0]
         self.beta = fit_parameters[1]
+
+        self.maximum_split = self.optimize_split(self.maximum_split)
+        self.zn_plus_split, self.mw_plus_split = self.split(self.scn_start, self.scn_plus_start, self.maximum_split, self.h, self.alpha, self.beta, self.lower_bound)
+
+        scn_split_full = np.arange(self.scn_start, self.maximum_split + 1)
+        self.split_fit_mi_full = np.array(scn_split_full * 14) + h_in
+        x_cdf_fit = self.split_fit_mi_full + 7 - self.lower_bound
+        zni_fit_cdf = gamma.cdf(x_cdf_fit / self.beta, self.alpha)
+        zni_fit_cdf[1:] -= zni_fit_cdf[:-1].copy()
+        self.split_fit_zni_full = zni_fit_cdf
+
+    def report(self):
+        print('Optimized Parameters: ')
+        print('>> Optimized Split SCN: ' + str(self.maximum_split))
+        print('>> Bound: ' + str(self.lower_bound))
+        print('>> Alpha: ' + str(self.alpha))
+        print('>> Beta: ' + str(self.beta))
+        print('>> h value used for Mi Calculations: ' + str(self.h))
+
+    def plot(self):
+        plt.scatter(self.mi_char_lab, self.zni_char_lab, label='Lab Data')
+        plt.plot(self.split_fit_mi_full, self.split_fit_zni_full, label='Fit Data')
+        plt.scatter(self.mw_plus_lab, self.zn_plus_lab, label='Lab Plus Fraction')
+        plt.scatter(self.mw_plus_split, self.zn_plus_split, label='Fit Plus Fraction')
+        plt.xlabel('Molecular Weight')
+        plt.ylabel('Normalized Mole Fraction')
+        plt.xlim(0)
+        plt.ylim(0)
+        plt.legend()
+        plt.show()        
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-PhaseOpt is a set of tools for Reservoir Fluid Charachterization.
+# PhaseOpt is a set of tools for Reservoir Fluid Charachterization
+
 So far, the ready tools is the C7+ splitting tool using Gamma Function. Will try to convert to a Class.
